33d5b06949484e3c29f4e1da23b2b82d6ad3250f
asultan00 <asultan00@stuy.edu>
2018-10-15 17:39:04 -0400

    edited Rational/RealNumber.java

---
+++ RationalNumber.java
@@ -0,0 +1,12 @@
+public class RationalNumber extends RealNumber {
+     private int top;
+     private int bottom;
+     public RationalNumber(int n, int d) {
+          super(1.0 * (a / b));
+          top = n;
+          bottom = d;
+     }
+     public boolean greaterThan(RationalNumber other) {
+          return true;
+     }
+}
---
+++ RealNumber.java
@@ -0,0 +1,12 @@
+public class RealNumber {
+     private double value;
+     public RealNumber(double v) {
+          value = v;
+     }
+     public double getValue() {
+          return 1.0;
+     }
+     public boolean greaterThan(RealNumber othern) {
+          return true;
+     }
+}
