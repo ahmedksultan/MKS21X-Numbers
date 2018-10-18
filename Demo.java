7fdb5c3a6a4e4984d3f811d10a8c77d69fd08425
asultan00 <asultan00@stuy.edu>
2018-10-17 20:37:49 -0400

    finished tasks

--- Demo.java
+++ Demo.java
@@ -1,28 +1,36 @@
 public class Demo {
      public static void main(String[] args) {
-          //realA = 8.0, realB = 4.0
+          //realnumber testing
           RealNumber realA;
           RealNumber realB;
           realA = new RealNumber(8.0);
           realB = new RealNumber(4.0);
-          System.out.println(realA); //8.0
-          System.out.println(realB); //4.0
-          System.out.println(realA.add(realB)); //12.0
-          System.out.println(realA.subtract(realB)); //4.0
-          System.out.println(realA.multiply(realB)); //32.0
-          System.out.println(realA.divide(realB)); //2.0
+          System.out.println(realA); // 8.0
+          System.out.println(realB); // 4.0
+          System.out.println(realA.add(realB)); // 12.0
+          System.out.println(realA.subtract(realB)); // 4.0
+          System.out.println(realA.multiply(realB)); // 32.0
+          System.out.println(realA.divide(realB)); // 2.0
           System.out.println("");
-          //ratA = 2 / 5, ratB = 6 / 10
+          //rationalnumber testing
           RationalNumber ratA;
-          RationalNumber ratB;
           RationalNumber ratA2;
+          RationalNumber ratB;
+          RationalNumber ratC;
           ratA = new RationalNumber(2, 5);
           ratA2 = new RationalNumber(2, 5);
           ratB = new RationalNumber(6, 10);
-          System.out.println(ratA); //2/5
-          System.out.println(ratB); //6/10
-          System.out.println(ratA.getValue()); //0.4
-          System.out.println(ratB.getValue()); //0.6
-          System.out.println(ratA.equals(ratA2)); //true
+          ratC = new RationalNumber(5, 0);
+          System.out.println(ratA); // 2/5
+          System.out.println(ratB); // 6/10
+          System.out.println(ratC); // 0/1
+          System.out.println(ratA.getValue()); // 0.4
+          System.out.println(ratB.getValue()); // 0.6
+          System.out.println(ratA.equals(ratA2)); // true
+          System.out.println(ratA.reciprocal()); // 5/2
+          System.out.println(ratA.multiply(ratB)); // 12/50
+          System.out.println(ratA.divide(ratB)); // 20/30
+          System.out.println(ratA.add(ratB)); // 10/10
+          System.out.println(ratA.subtract(ratB)); // -2/10
      }
 }
--- RationalNumber.java
+++ RationalNumber.java
@@ -1,37 +1,79 @@
 public class RationalNumber extends RealNumber {
-  private int numerator, denominator;
-  public RationalNumber(int nume, int deno) {
-    super(0);//this value is ignored!
-    numerator = nume;
-    if (deno == 0) {
-         numerator = 0;
-         denominator = 1;
-    }
-    else {
-         denominator = deno;
-    }
-  }
-  public double getValue() {
-       return (double)numerator / denominator;
- }
-  public int getNumerator(){
-    return numerator;
-  }
-  public int getDenominator(){
-    return denominator;
-  }
-  /*
-  public RationalNumber reciprocal(){
-    RationalNumber recip;
-    recip = new RationalNumber(int deno, int nume);
-} */
-  public boolean equals(RationalNumber other){
-    if ( numerator == other.getNumerator() && denominator == other.getDenominator() ) {
-         return true;
-    }
-    return false;
-  }
-  public String toString(){
-    return numerator + "/" + denominator;
-  }
+     private int numerator, denominator;
+     public RationalNumber(int nume, int deno) {
+          super(0);//this value is ignored!
+          numerator = nume;
+          if (deno == 0) {
+              numerator = 0;
+              denominator = 1;
+          }
+          else {
+              denominator = deno;
+          }
+     }
+     public double getValue() {
+          return (double)numerator / denominator;
+      }
+     public int getNumerator() {
+          return numerator;
+     }
+     public int getDenominator() {
+          return denominator;
+     }
+     public boolean equals(RationalNumber other) {
+          if ( numerator == other.getNumerator() &&
+           denominator == other.getDenominator() ) {
+               return true;
+          }
+          return false;
+     }
+     public String toString() {
+          return numerator + "/" + denominator;
+     }
+     public RationalNumber reciprocal() {
+          RationalNumber temp;
+          if (numerator == 0) {
+               temp = new RationalNumber(0, 1);
+          }
+          else {
+               temp = new RationalNumber(denominator, numerator);
+          }
+          return temp;
+     }
+     /*credit goes to https://introcs.cs.princeton.edu/java/23recursion/Euclid.java.html
+     for explaining the problem and helping to understand it*/
+     private static int gcd(int a, int b) {
+          while (b != 0) {
+               int c = b;
+               b = a % b;
+               a = c;
+          }
+          return a;
+     }
+     //Alma helped me with reduce!
+     private void reduce() {
+          int ratgcd = gcd(numerator, denominator);
+          numerator = numerator / ratgcd;
+          denominator = denominator / ratgcd;
+     }
+     public RationalNumber multiply(RationalNumber other) {
+          RationalNumber result;
+          result = new RationalNumber((numerator * other.getNumerator()), (denominator * other.getDenominator()));
+          return result;
+     }
+     public RationalNumber divide(RationalNumber other) {
+          RationalNumber result;
+          result = new RationalNumber((numerator * other.getDenominator()), (denominator * other.getNumerator()));
+          return result;
+     }
+     public RationalNumber add(RationalNumber other) {
+          RationalNumber result;
+          result = new RationalNumber(((numerator * other.getDenominator()) + (denominator * other.getNumerator())), (denominator * other.getDenominator()));
+          return result;
+     }
+     public RationalNumber subtract(RationalNumber other) {
+          RationalNumber result;
+          result = new RationalNumber(((numerator * other.getDenominator()) - (denominator * other.getNumerator())), (denominator * other.getDenominator()));
+          return result;
+     }
 }
